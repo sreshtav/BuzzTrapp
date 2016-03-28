0fbb19f081fce68dcc5eb5fdd5d27e7f4a46b87b
Aaron Quek <aaronquekweiyang@hotmail.com>
2016-03-28 12:32:30 -0400

    Add cancel button in sign up page to go back to log in page

--- android/Trapp/app/src/main/java/buzztrapp/trapp/SignUpActivity.java
+++ android/Trapp/app/src/main/java/buzztrapp/trapp/SignUpActivity.java
@@ -1,7 +1,9 @@
 package buzztrapp.trapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_activity);
+    }
+
+    public void launchSignInActivity(View view){
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
