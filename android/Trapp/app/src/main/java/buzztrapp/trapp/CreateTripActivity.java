263e8bfa97b77cf84b6278afbf59d489cffa7af8
Aaron Quek <aaronquekweiyang@hotmail.com>
2016-03-29 23:58:57 -0400

    Added Tabbed Activity for Create Trips

--- android/Trapp/app/src/main/java/buzztrapp/trapp/CreateTripActivity.java
+++ android/Trapp/app/src/main/java/buzztrapp/trapp/CreateTripActivity.java
@@ -1,7 +1,10 @@
 package buzztrapp.trapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class CreateTripActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class CreateTripActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_trip_activity);
+
+        getSupportActionBar().setTitle("Manage Trips");
+
+
+        TabHost createTripsTabs = (TabHost)findViewById(R.id.ct_tabhost);
+
+        TabSpec destination_tab = createTripsTabs.newTabSpec("Destination");
+        TabSpec duration_tab = createTripsTabs.newTabSpec("Duration");
+
+        destination_tab.setIndicator("Tab1");
+        destination_tab.setContent(new Intent(this,CreateTripDestinationActivity.class));
+
+        duration_tab.setIndicator("Tab2");
+        duration_tab.setContent(new Intent(this,CreateTripDurationActivity.class));
+
     }
 }
