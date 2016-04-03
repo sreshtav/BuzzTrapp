6ec81f17f77785b2c9ee3c4386158f677da6614b
Aaron Quek <aaronquekweiyang@hotmail.com>
2016-04-03 16:57:12 -0400

    Added CalendarView for Create Trip

--- android/Trapp/app/src/main/java/buzztrapp/trapp/CreateTripDurationFragment.java
+++ android/Trapp/app/src/main/java/buzztrapp/trapp/CreateTripDurationFragment.java
@@ -9,13 +9,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class CreateTripDurationFragment extends android.support.v4.app.Fragment {
 
     public static final String ARG_OBJECT = "object";
-
+    CalendarPickerView calendar;
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.create_trip_duration, container, false);
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+
+        calendar = (CalendarPickerView) getActivity().findViewById(R.id.calendar_view);
+        Date today = new Date();
+        calendar.init(today, nextYear.getTime()).inMode(CalendarPickerView.SelectionMode.RANGE);
     }
 }
