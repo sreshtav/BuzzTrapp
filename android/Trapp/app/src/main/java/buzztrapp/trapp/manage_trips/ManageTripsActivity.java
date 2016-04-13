cea0473d80ee9e6c2924479093df62b1b26d54cf
Nisheeth Bandaru <nisheeth@gatech.edu>
2016-04-13 13:11:56 -0400

    notifications added
    alarm still doesnt work

--- android/Trapp/app/src/main/java/buzztrapp/trapp/manage_trips/ManageTripsActivity.java
+++ android/Trapp/app/src/main/java/buzztrapp/trapp/manage_trips/ManageTripsActivity.java
@@ -4,6 +4,16 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+
+import android.support.v4.app.NotificationCompat;
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import java.util.GregorianCalendar;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,6 +26,7 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import buzztrapp.trapp.AlertReceiver;
 import buzztrapp.trapp.Communicator;
 import buzztrapp.trapp.R;
 import buzztrapp.trapp.create_trip.CreateTripActivity;
@@ -27,6 +38,15 @@ public class ManageTripsActivity extends AppCompatActivity implements Communicat
     private List<Trip> fullTripsList;
     private ManageTripsRVAdapter adapter;
     private int noOfTrips = 0;
+
+    // Allows us to notify the user that something happened in the background
+    NotificationManager notificationManager;
+
+    // Used to track notifications
+    int notifID = 8803;
+
+    // Used to track if notification is active in the task bar
+    boolean isNotificActive = false;
 
     ManageTripsFragment contentFragment;
     FragmentManager manager;
@@ -48,6 +68,7 @@ public class ManageTripsActivity extends AppCompatActivity implements Communicat
 
         getSupportActionBar().setTitle("Manage Trips");
         Log.d("Aaron", "Main: onCreate and about to add contentFragment");
+
 
 
 
@@ -83,6 +104,8 @@ public class ManageTripsActivity extends AppCompatActivity implements Communicat
             public void onDrawerOpened(View drawerView) {
                 Toast.makeText(ManageTripsActivity.this, "Drawer Closed",
                         Toast.LENGTH_SHORT).show();
+                showNotification();
+
             }
 
             @Override
@@ -107,5 +130,76 @@ public class ManageTripsActivity extends AppCompatActivity implements Communicat
 
     @Override
     public void respond(String data) {
+    }
+
+    public void setAlarm() {
+
+        // Define a time value of 5 seconds
+        Long alertTime = new GregorianCalendar().getTimeInMillis()+5*1000;
+
+        // Define our intention of executing AlertReceiver
+        Intent alertIntent = new Intent(this, AlertReceiver.class);
+
+        // Allows you to schedule for your application to do something at a later date
+        // even if it is in he background or isn't active
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        // set() schedules an alarm to trigger
+        // Trigger for alertIntent to fire in 5 seconds
+        // FLAG_UPDATE_CURRENT : Update the Intent if active
+        alarmManager.set(AlarmManager.RTC_WAKEUP, alertTime,
+                PendingIntent.getBroadcast(this, 1, alertIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT));
+
+    }
+
+    public void showNotification() {
+
+        // Builds a notification
+        NotificationCompat.Builder notificBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("Stop by for Lunch?")
+                .setContentText("A highly rated Indian restaurant Touch is 1.1 miles away. Tap here to go there")
+                .setTicker("Stop by for Lunch?")
+                .setSmallIcon(R.drawable.trapp_icon_1);
+
+        // Define that we have the intention of opening MoreInfoNotification
+        Intent moreInfoIntent = new Intent(this, ManageTripsActivity.class);
+
+        // Used to stack tasks across activites so we go to the proper place when back is clicked
+        TaskStackBuilder tStackBuilder = TaskStackBuilder.create(this);
+
+        // Add all parents of this activity to the stack
+        tStackBuilder.addParentStack(ManageTripsActivity.class);
+
+        // Add our new Intent to the stack
+        tStackBuilder.addNextIntent(moreInfoIntent);
+
+        // Define an Intent and an action to perform with it by another application
+        // FLAG_UPDATE_CURRENT : If the intent exists keep it but update it if needed
+        PendingIntent pendingIntent = tStackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Defines the Intent to fire when the notification is clicked
+        notificBuilder.setContentIntent(pendingIntent);
+
+        // Gets a NotificationManager which is used to notify the user of the background event
+        notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Post the notification
+        notificationManager.notify(notifID, notificBuilder.build());
+
+        // Used so that we can't stop a notification that has already been stopped
+        isNotificActive = true;
+
+    }
+
+    public void stopNotification() {
+
+        // If the notification is still active close it
+        if(isNotificActive) {
+            notificationManager.cancel(notifID);
+        }
+
     }
 }
