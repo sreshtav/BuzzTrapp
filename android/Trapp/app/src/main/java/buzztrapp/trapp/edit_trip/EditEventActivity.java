0464035e2e0c5098348de918facc80de8fef0427
Aaron Quek <aaronquekweiyang@hotmail.com>
2016-04-16 16:20:32 -0400

    Edit Event Activity Commit

--- android/Trapp/app/src/main/java/buzztrapp/trapp/edit_trip/EditEventActivity.java
+++ android/Trapp/app/src/main/java/buzztrapp/trapp/edit_trip/EditEventActivity.java
@@ -25,21 +25,21 @@ public class EditEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_event_activity);
 
-       /* Intent inIntent = getIntent();
+        Intent inIntent = getIntent();
         Bundle inBundle = inIntent.getExtras();
 
         name = inBundle.getString("name");
         location = inBundle.getString("location");
         GregorianCalendar startDate = (GregorianCalendar) inBundle.getSerializable("startTime");
-        GregorianCalendar endDate = (GregorianCalendar) inBundle.getSerializable("endTime");*/
+        GregorianCalendar endDate = (GregorianCalendar) inBundle.getSerializable("endTime");
 
-       /* TextView tv = (TextView)findViewById(R.id.ee_textView);
+        TextView tv = (TextView)findViewById(R.id.ee_textView);
 
         if (name.equals("") && location.equals("")){
             tv.setText("No Event found! Create one now! Start time = "+startDate.getTime().toString() +", end time = "+endDate.getTime().toString());
         }
         else{
             tv.setText("Event details: name = "+name + ", location = "+location + ", start time = "+startDate.getTime().toString() +", end time = "+endDate.getTime().toString());
-        }*/
+        }
     }
 }
